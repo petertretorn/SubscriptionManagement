f65187c8d67910d03c61c81d13b60ef80c440762
Peter Larsen <petertretorn@gmail.com>
2021-03-12 20:58:15 +0100

    added Subscription controller to API project

---
+++ SubscriptionManagement.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SubscriptionManagement.Application.Features.AddSubscription;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubscriptionManagement.Api.Controllers
+{
+    [Route("api/[controller]/{subscriptionId}")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SubscriptionController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost(Name = "AddSubscription")]
+        public async Task<ActionResult<Guid>> Create([FromBody] AddSubscriptionCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
