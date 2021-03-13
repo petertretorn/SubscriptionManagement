ec90ce614dc28db37d8f0178d86688b360495784
Peter Larsen <petertretorn@gmail.com>
2021-03-13 10:03:12 +0100

    added Entity base type

---
+++ SubscriptionManagement.Domain/Util/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManagement.Domain.Util
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+    }
+}
